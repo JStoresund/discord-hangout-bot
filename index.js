cf6bc9872b40a8ee11e2b3b30497a3594478e24a
Joresund <johanstore2002@gmail.com>
2024-02-18 22:10:16 +0100

    feat: first discord slash command

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import { REST, Routes, SlashCommandBooleanOption, SlashCommandBuilder } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
-import { Client, GatewayIntentBits, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle} from 'discord.js'
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.DirectMessages
-  ]
-})
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
-client.on("messageCreate", async (message) => {
-  console.log(message)
+try {
+  console.log('Started refreshing application (/) commands.');
 
-  if (message.author.bot) return
-  message.author.send({
-    content: "Hello!",
-    components: [
-      new ButtonBuilder()
-        .setStyle(ButtonStyle.PRIMARY)
-        .setLabel("Click me!")
-        .setCustomId("click_me")
-        .toJSON()
-    ]
-  })
-})
+  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
+
+
+
+
+
+import { Client, GatewayIntentBits } from 'discord.js';
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async (interaction, args) => {
+  console.log(args)
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
