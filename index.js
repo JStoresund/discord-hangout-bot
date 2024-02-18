fcba1ec8f56a037d2c41797babca947a0e2d8f89
Joresund <johanstore2002@gmail.com>
2024-02-18 15:28:46 +0100

    feat: basic bot functionality

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import { Client, GatewayIntentBits } from 'discord.js'
+import { Client, GatewayIntentBits, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle} from 'discord.js'
 
 const client = new Client({
   intents: [
@@ -14,4 +14,16 @@ const client = new Client({
 
 client.on("messageCreate", async (message) => {
   console.log(message)
+
+  if (message.author.bot) return
+  message.author.send({
+    content: "Hello!",
+    components: [
+      new ButtonBuilder()
+        .setStyle(ButtonStyle.PRIMARY)
+        .setLabel("Click me!")
+        .setCustomId("click_me")
+        .toJSON()
+    ]
+  })
 })
