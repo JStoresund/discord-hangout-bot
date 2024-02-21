0667534446ab7178ea319a3829771446ed1ac75e
Joresund <johanstore2002@gmail.com>
2024-02-21 21:34:07 +0100

    refactor: changed from module to commonjs in order to provide argument support in commands

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import { REST, Routes, SlashCommandBooleanOption, SlashCommandBuilder } from 'discord.js';
-import dotenv from 'dotenv';
-dotenv.config();
+require("dotenv").config()
 
-import { Client, GatewayIntentBits } from 'discord.js';
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [
+  GatewayIntentBits.Guilds, 
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent
+] });
 
 const commands = [
   {
@@ -28,21 +30,6 @@ const commands = [
   }
 ]
 
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
-
-try {
-  console.log('Started refreshing application (/) commands.');
-
-  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
-
-  console.log('Successfully reloaded application (/) commands.');
-} catch (error) {
-  console.error(error);
-}
-
-
-
-
 const decomposeCommand = (command) => {
   const splitCommands = command.split(' ')
   let commandName = splitCommands[0]
@@ -59,43 +46,25 @@ const decomposeCommand = (command) => {
   throw "ERROR: Invalid command"
 }
 
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
+})
 
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  try {
-    const { commandName, commandArgs } = decomposeCommand(interaction.commandName)
+client.on('messageCreate', async (msg) => {
+  console.log("Function called")
+  // console.log(msg.author)
+  if (msg.author.bot) return;
+  console.log(msg)
+  if (msg.content === 'ping') {
+    msg.reply({
+      content: 'Pong!'
+    });
   }
-  catch (error) {
-    interaction.reply(error);
-    return
+  else if (msg.content === '') {
+    msg.reply('No message recieved')
   }
+})
 
-  if (commandName === 'ping') {
-    await interaction.reply('Pong!');
-    return
-  }
-
-  if (commandName === 'help') {
-    let reply = 'Here are all the available commands:\n\n'
-    for(const command of commands) {
-      reply += `**/${command.name}**\n ${command.description}\n\n`
-    }
-    await interaction.reply(reply);
-    return
-  }
-
-  if (commandName === 'test') {
-    await interaction.reply('Test command');
-    return
-  }
-
-  if (commandName === 'hello') {
-    await interaction.reply(`Hello, ${commandArgs[0]}!`);
-    return
-  }
-});
 
 client.login(process.env.DISCORD_TOKEN);
