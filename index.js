8399bf57b5c3073698156ecb7346fb2b32ef1dbc
Joresund <johanstore2002@gmail.com>
2024-02-21 20:15:05 +0100

    feat: function to decompose commands

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ import { REST, Routes, SlashCommandBooleanOption, SlashCommandBuilder } from 'di
 import dotenv from 'dotenv';
 dotenv.config();
 
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-];
-
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 try {
@@ -28,17 +21,76 @@ try {
 import { Client, GatewayIntentBits } from 'discord.js';
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+    arguments: 0,
+  },
+  {
+    name: 'hello',
+    description: 'Lists all available commands',
+    arguments: 0,
+  },
+  {
+    name: 'help',
+    description: 'Says hello to the given name',
+    arguments: 1,
+  },
+  {
+    name: 'test',
+    description: 'Test command',
+    arguments: 0,
+  }
+]
+
+const decomposeCommand = (command) => {
+  const splitCommands = command.split(' ')
+  let commandName = splitCommands[0]
+  let commandArgs = splitCommands.slice(1)
+  for(const command of commands) {
+    if (command.name !== commandName) constinue
+    if (command.arguments !== commandArgs.length) {
+      console.log('Invalid number of arguments')
+      return "ERROR: Invalid number of arguments"
+    }
+    return { commandName, commandArgs }
+  }
+  return "ERROR: Invalid command"
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async (interaction, args) => {
-  console.log(args)
+client.on('interactionCreate', async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
+  const { commandName, commandArgs } = decomposeCommand(interaction.commandName)
 
-  if (interaction.commandName === 'ping') {
+  if (commandName === 'ping') {
     await interaction.reply('Pong!');
+    return
   }
+
+  if (commandName === 'help') {
+    let reply = 'Here are all the available commands:\n\n'
+    for(const command of commands) {
+      reply += `**/${command.name}**\n ${command.description}\n\n`
+    }
+    await interaction.reply(reply);
+    return
+  }
+
+  if (commandName === 'test') {
+    await interaction.reply('Test command');
+    return
+  }
+
+  if (commandName === 'hello') {
+    await interaction.reply(`Hello, ${commandArgs[0]}!`);
+    return
+  }
+  await interaction.reply('Invalid command');
 });
 
 client.login(process.env.DISCORD_TOKEN);
