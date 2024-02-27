b2116b5a6c4d8d37395378502ffbfd4d6e8de243
Joresund <johanstore2002@gmail.com>
2024-02-27 20:47:15 +0100

    feat: first command

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const {DISCORD_TOKEN} = require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
