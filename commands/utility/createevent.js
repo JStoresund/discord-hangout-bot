6553d2236a771d4e40eb0f38ddc1047cddd7b42d
Joresund <johanstore2002@gmail.com>
2024-03-28 19:43:45 +0100

    feat: baseline event creation

--- commands/utility/createevent.js
+++ commands/utility/createevent.js
@@ -1,29 +1,41 @@
-const { SlashCommandBuilder } = require('@discordjs/builders')
+const { SlashCommandBuilder, italic } = require('@discordjs/builders')
+const { REST } = require('@discordjs/rest')
 const { guildID } = require('../../config.json')
-const { Routes, RESTPostAPIGuildScheduledEventJSONBody, RESTPostAPIGuildScheduledEventResult } = require('discord-api-types/v9');
+const { createDiscordEvent } = require('../../event.js')
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('event')
+    .setName('createevent')
     .setDescription('Creates a new event!')
+
     .addStringOption(option => 
       option.
         setName('name')
         .setDescription('The name of the event')
         .setRequired(true)
       )
+
     .addStringOption(option => 
       option.
         setName('date')
         .setDescription('The date of the event in the format MM-DD OR MM-DD-YYYY')
         .setRequired(true)
       )
+
     .addStringOption(option => 
       option.
         setName('time')
         .setDescription('The time of the event in the format HH:MM')
         .setRequired(true)
+      )
+
+    .addStringOption(option =>
+      option.
+        setName('description')
+        .setDescription('A description of the event')
+        .setRequired(false)
       ),
+
   async execute(interaction) {
     const name = interaction.options.getString('name')
     let date = interaction.options.getString('date').split('-')
@@ -33,10 +45,15 @@ module.exports = {
     }
     let time = interaction.options.getString('time').split(':')
 
-    console.log(`Name: ${name}`)
-    console.log(`Date: ${date}`)
-    console.log(`Time: ${time}`)
-    await interaction.reply(`Event created: ${name} on ${date[0]}/${date[1]}/${date[2]} at ${time[0]}:${time[1]}`)
+    const description = interaction.options.getString('description') || 'No description provided.'
 
+    const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
+
+    const startTime = new Date(2024, 3)
+    const endTime = new Date(2024, 4)
+
+    await createDiscordEvent(name, startTime, endTime, 'https://discord.com')
+
+    await interaction.reply(`Code finished running!`)
   },
 }
